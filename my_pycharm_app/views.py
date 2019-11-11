abd1106476719e2b344df6af75364d2e21066486
Tyrone Hunt <tyrone.hunt07@gmail.com>
2019-11-11 22:36:27 +0000

    save point prior to attempting jinja2 to chameleon switch

--- my_pycharm_app/views.py
+++ my_pycharm_app/views.py
@@ -1,6 +1,10 @@
 from pyramid.view import view_config
-
+import my_pycharm_app.utils
 
 @view_config(route_name='home', renderer='templates/mytemplate.jinja2')
 def my_view(request):
     return {'project': 'my_pycharm_app'}
+
+def extend_model(model_dict):
+    model_dict['build_cache_id'] = my_pycharm_app.utils.build_cache_id
+    return model_dict
