34aef23eeb8ef22ad50e89fecbb84bd9e5b35471
Tyrone Hunt <tyrone.hunt07@gmail.com>
2019-11-16 12:44:25 +0000

    still debugging pyramid framework errors. Committing fixes to views, routes and __init__ before next round of debugging

--- pycharm_app/__init__.py
+++ pycharm_app/__init__.py
@@ -4,8 +4,20 @@ from pyramid.config import Configurator
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
-    with Configurator(settings=settings) as config:
-        config.include('pyramid_chameleon')
-        config.include('.routes')
-        config.scan()
+    config = Configurator(settings=settings)
+    config.include('pyramid_chameleon')
+
+    # Set up the static folder
+    config.add_static_view('static', 'static', cache_max_age=3600)
+
+    # Set up the template folder routes
+    config.add_route('home', '/')
+    config.add_route('layout', '/layout')
+
+    # Reference other integrations
+    cfg_settings = config.get_settings()
+    db_file = cfg_settings.get('db_file')
+    api_key = cfg_settings.get('api_key')
+    config.scan()
+
     return config.make_wsgi_app()
--- pycharm_app/views.py
+++ pycharm_app/views.py
@@ -2,20 +2,39 @@ from pyramid.view import view_config
 import pycharm_app.static_cache
 
 
-@view_config(route_name='mytemplate', renderer='templates/mytemplate.pt')
-def index(_):
+@view_config(route_name='home', renderer='templates/mytemplate.pt')
+def my_view(request):
     return extend_model({'project': 'pycharm_app'})
+
+
+def index(request):
+    albums = [
+        {'has_preview': True, 'title': 'song_name_here', 'url': 'album/123'},
+        {'has_preview': False, 'title': 'song_name_here2', 'url': 'album/124'}
+    ]
+    return {'albums': albums}
 
 
 @view_config(route_name='layout', renderer='templates/layout.pt')
-def layout(_):
-    return extend_model({'project': 'pycharm_app'})
+def layout(request):
+    return extend_model({})
 
+
+# @view_config(route_name='reset_post', request_method='POST', renderer='templates/reset_password.jinja2')
+# def reset_password(request):
+#     # Collect data from route, querystring, POST etc.
+#     email = request.POST.get('email')
+#
+#     # Process request
+#     if not repository.create_reset_password(email):
+#         return {'error': "Cannot reset password"}
+#
+#     # Return model to template engine
+#     return {
+#             'error': None,
+#             'msg': 'Check your email for reset code'
+#            }
 
 def extend_model(model_dict):
-    print("extend_model called on", model_dict)
     model_dict['build_cache_id'] = pycharm_app.static_cache.build_cache_id
-    print("model_dict returned as:", model_dict)
     return model_dict
-
-extend_model({'project': 'pycharm_app'})
